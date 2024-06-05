63305fd1ada106150ad59cb04012614fa8ef694f
zvdv <156561301+zvdv@users.noreply.github.com>
2024-06-04 20:47:29 -0700

    Basic Model

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 # pro tip: the target network and the network network need to have the 
 # exact same architecture otherwise you cannot copy the weights between them.
@@ -10,5 +12,14 @@ def Model():
     '''this shouldn't need to be a class. this model should be very simple.
     especially for cartpole, like, 2 dense, an output, and no convolutions 
     should be more than enough.'''
+
+    model = nn.Sequential(
+        nn.Linear(4, 18),
+        nn.ReLU(),
+        nn.Linear(18, 10),
+        nn.ReLU(),
+        nn.Linear(10, 1),
+        nn.Sigmoid()
+    )
 
     return
